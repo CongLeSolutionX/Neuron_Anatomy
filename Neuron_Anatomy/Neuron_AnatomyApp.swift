e338a53cd2fa11d782126cfad0447002544582d7
Cong Le <longchike@gmail.com>
2025-06-29 03:04:57 -0700

    The setup

---
+++ Neuron_Anatomy/Neuron_AnatomyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neuron_AnatomyApp.swift
+//  Neuron_Anatomy
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Neuron_AnatomyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
